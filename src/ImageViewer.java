8b5f5fcdfd92f9ee181ac210db8d1cf33b45e203
malthe <malthe@localhost.localdomain>
2017-03-14 17:47:03 +0100

    initial commit

---
+++ src/ImageViewer.java
@@ -0,0 +1,71 @@
+import org.opencv.core.Mat;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+
+/**
+ * Created by malthe on 3/10/17.
+ */
+public class ImageViewer {
+    private JLabel imageView;
+    private JFrame frame;
+    public ImageViewer() {
+        createJFrame("");
+    }
+
+    public void show(Mat image) {
+        show(image,"");
+    }
+    public void show(Mat image,String windowName){
+        setSystemLookAndFeel();
+        //JFrame frame = createJFrame(windowName);
+        Image loadedImage = toBufferedImage(image);
+        imageView.setIcon(new ImageIcon(loadedImage));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    private JFrame createJFrame(String windowName) {
+        frame = new JFrame(windowName);
+        imageView = new JLabel();
+        final JScrollPane imageScrollPane = new JScrollPane(imageView);
+        imageScrollPane.setPreferredSize(new Dimension(640, 480));
+        frame.add(imageScrollPane, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        return frame;
+    }
+
+    private void setSystemLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel
+                    (UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Image toBufferedImage(Mat matrix){
+        int type = BufferedImage.TYPE_BYTE_GRAY;
+        if ( matrix.channels() > 1 ) {
+            type = BufferedImage.TYPE_3BYTE_BGR;
+        }
+        int bufferSize = matrix.channels()*matrix.cols()*matrix.rows();
+        byte [] buffer = new byte[bufferSize];
+        matrix.get(0,0,buffer); // get all the pixels
+        BufferedImage image = new BufferedImage(matrix.cols(),matrix.
+                rows(), type);
+        final byte[] targetPixels = ((DataBufferByte) image.getRaster().
+                getDataBuffer()).getData();
+        System.arraycopy(buffer, 0, targetPixels, 0, buffer.length);
+        return image;
+    }
+}
---
+++ src/OpenCV.java
@@ -0,0 +1,90 @@
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import javax.swing.text.html.ImageView;
+
+import static org.opencv.imgproc.Imgproc.circle;
+import static org.opencv.imgproc.Imgproc.rectangle;
+
+/**
+ * Created by malthe on 3/8/17.
+ */
+public class OpenCV {
+
+    static{System.loadLibrary(Core.NATIVE_LIBRARY_NAME);}
+    public static ImageViewer viewer = new ImageViewer();
+    public static void main(String[] args) {
+
+        for (int i = 141; i <643; i++) {
+            String imagepath = "Resources/billed/image"+i+".jpg";
+            detectAndShowCircles(imagepath);
+            try {
+                //1 fps pcmasterrace
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        }
+    private static void detectAndShowCircles(String imagepath) {
+        Mat image = Imgcodecs.imread(imagepath);
+        Mat gray = new Mat();
+        Mat circles = new Mat();
+
+        Imgproc.cvtColor(image, gray, Imgproc.COLOR_BGR2GRAY);
+
+        Imgproc.HoughCircles(gray, circles, Imgproc.CV_HOUGH_GRADIENT, 1, 60, 200, 20, 30, 0 );
+        System.out.println("#rows " + circles.rows() + " #cols " + circles.cols());
+        double x = 0.0;
+        double y = 0.0;
+        int r = 0;
+
+        for( int i = 0; i < circles.rows(); i++ ) {
+            double[] data = circles.get(i, 0);
+            for (int j = 0; j < data.length; j++) {
+                x = data[0];
+                y = data[1];
+                r = (int) data[2];
+            }
+            Point center = new Point(x, y);
+            pointFeaturesToCircleCenter(image, center);
+            // circle center
+            circle(image, center, 3, new Scalar(0, 255, 0), -1);
+            // circle outline
+            circle(image, center, r, new Scalar(0, 0, 255), 1);
+            viewer.show(image);
+
+            //Core.
+        }
+    }
+
+    private static MatOfPoint pointFeaturesToCircleCenter(Mat image, Point center) {
+        MatOfPoint corners = new MatOfPoint();
+        //corners.
+        Mat gray = new Mat();
+        Imgproc.cvtColor(image, gray, Imgproc.COLOR_BGR2GRAY);
+        //this function only works on gray scale imgs. it will throw exception if colored.
+        //will identify points in the image
+        Imgproc.goodFeaturesToTrack(gray, corners, 100, 0.1, 0.0);
+        Point[] cornerpoints = corners.toArray();
+
+        int x = 1;
+        for (Point points : cornerpoints) {
+            //draw vectors from identified image features to the circle center
+            Imgproc.line(image, points, center, new Scalar( 0, 255, 0, 128 ) );
+        }
+
+        //draw corners
+
+        //rectangle(image, new Point(cornerpoints[0].x, cornerpoints[0].y), new Point(cornerpoints[1].x, cornerpoints[1].y), new Scalar(255, 255, 0));
+        //Imgproc.goodFeaturesToTrack();
+        return corners;
+    }
+}
+
+
+
+
+
